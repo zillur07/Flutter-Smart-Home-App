06c2d002880045f5e235208eab4bb08249b8ae4b
zillur07 <zillur.info1@gmail.com>
2023-06-15 04:43:07 +0600

    project all done

--- lib/src/pages/smart_home.dart
+++ lib/src/pages/smart_home.dart
@@ -366,9 +366,3 @@ class _SmartHomeState extends State<SmartHome> {
     );
   }
 }
-
-// class SalesData {
-//   SalesData(this.year, this.sales);
-//   final DateTime year;
-//   final double sales;
-// }
